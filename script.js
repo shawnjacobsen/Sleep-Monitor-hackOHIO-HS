b99a5e457a527ec7a95e777efc4b263d449f75ee
shaj684 <shaj684@gmail.com>
2019-03-23 14:34:03 -0400

    created functioning chart and data variables with user input

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-// x-axis
-var days = [1,2,3,4,5,6,7];
-// plot data
-var sleepHours = [6, 8, 5, 7, 8 ,5, 10];
-
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: days,
+var canvas = document.getElementById('myChart');
+var data = {
+    labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
     datasets: [
-      { 
-        data: sleepHours,
-        label: "Hours Slept"
-      }
+        {
+            label: "Hours You've Slept",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 5,
+            pointHitRadius: 10,
+            data: [5, 8, 4, 9, 8, 7, 9, 10],
+        }
     ]
-  }
+};
+
+function addData(newData){
+	myLineChart.data.datasets[0].data[data.labels.length] = Number(newData);
+	myLineChart.data.labels[data.labels.length] = "Day " + Number(data.labels.length + 1);
+	myLineChart.update();
+}
+
+var option = {
+	showLines: true,
+	responsive:true,
+	maintainAspectRatio: false
+};
+var myLineChart = Chart.Line(canvas,{
+	data:data,
+  options:option
 });
+
+var sleepInput = 0;
+function getUserInput() {
+    sleepInput = Number(document.getElementById("sleep-input").value);
+    console.log("sleepInput = " + sleepInput);
+    addData(sleepInput);
+}
