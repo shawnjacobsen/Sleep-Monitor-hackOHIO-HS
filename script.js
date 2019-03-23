8022b1cd94a90524999ffa3f2b78d8c27d5cfc4e
shaj684 <shaj684@gmail.com>
2019-03-23 12:59:27 -0400

    adding style of canvas id width and height

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//testing script for javascript
 console.log("test");
+
+// x-axis
+var days = [1,2,3,4,5,6,7];
+// plot data
+var sleepHours = [6, 8, 5, 7, 8 ,5, 10];
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: days,
+    datasets: [
+      { 
+        data: sleepHours,
+        label: "Hours Slept"
+      }
+    ]
+  }
+});
