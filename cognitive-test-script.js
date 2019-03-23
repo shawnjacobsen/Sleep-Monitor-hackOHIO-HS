c4f8628e0e3e409f2123e4873c10641e4ba76138
shaj684 <shaj684@gmail.com>
2019-03-23 17:06:01 -0400

    creating final presenation (canvas and cognitive test is hidden)

--- cognitive-test-script.js
+++ cognitive-test-script.js
@@ -1,2 +1,31 @@
 // Script for testing cognitive ability
 canvas = document.getElementById("cognitive-test");
+
+var Circle1 = document.createElement('li');
+Circle1.innerHTML = '<div id="circle-1">HELLO</div>';
+canvas.appendChild(Circle1);
+
+randomInt = 0;
+function startTest() {
+	randomInt = Math.round(Math.random());
+	var ctx = canvas.getContext("2d");
+
+	ctx.beginPath();
+	ctx.arc(340, 120, 50, 0, 2 * Math.PI);
+	if (randomInt == 0) {
+		ctx.fillStyle = 'red';		
+	} else {
+		ctx.fillStyle = 'green';
+	}
+	ctx.fill();
+
+	var ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(60, 120, 50, 0, 2 * Math.PI);
+	if (randomInt == 0) {
+		ctx.fillStyle = 'green';		
+	} else {
+		ctx.fillStyle = 'red';
+	}
+	ctx.fill();
+}
